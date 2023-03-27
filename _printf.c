c65eb52ef5f8415073364c0ab2ae2d3e5c6bc12e
RealKingTino <tmowizzy@gmail.com>
2023-03-27 09:53:07 -0700

    Add _printf function to print strings and characters to output

---
+++ _printf.c
@@ -0,0 +1,69 @@
+#include "main.h"
+#include <unistd.h> /* for write function */
+
+/**
+ * _printf - prints output according to a format
+ * @format: the format string to be printed
+ *
+ * Return: the number of characters printed (excluding the null byte used
+ * to end output to strings), or -1 if an error occurred
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i, count = 0;
+	char *str;
+
+	va_start(args, format); /* initialize the va_list */
+	/* loop through the format string */
+	for (i = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
+	{
+			switch (format[i + 1])
+			{
+				case 'c': /* char */
+					i++;
+					count += write(1, &va_arg(args, int), 1);
+					break;
+				case 's': /* string */
+					i++;
+					str = va_arg(args, char *);
+					if (str == NULL)
+						count += write(1, "(null)", 6);
+					else
+						count += write(1, str, _strlen(str));
+					break;
+				case '%': /* percent sign */
+					i++;
+					count += write(1, "%", 1);
+					break;
+				default: /* handle unknown specifier */
+					i++;
+					count += write(1, "%", 1);
+					count += write(1, &format[i], 1);
+					break;
+			}
+		}
+		else /* handle regular character */
+			count += write(1, &format[i], 1);
+	}
+	va_end(args); /* clean up the va_list */
+	return (count);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to be measured
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s && s[i]; i++)
+		;
+
+	return (i);
+}
