79c318015014bfcd79f8c88d4208ce247ca70d20
RealKingTino <tmowizzy@gmail.com>
2023-03-29 12:58:23 -0700

    Refactor code based on QA Review

--- _printf.c
+++ _printf.c
@@ -15,6 +15,9 @@ int _printf(const char *format, ...)
 	int count = 0;
 	int i;
 
+	if (format == NULL)
+		return (-1);
+
 	va_start(args, format);
 	for (i = 0; format[i]; i++)
 	{
@@ -64,23 +67,8 @@ int print_c(va_list args)
 }
 
 /**
- * _strlen - gets the length of a string
- * @str: the string to get the length of
- *
- * Return: the length of the string
- */
-int _strlen(char *str)
-{
-	int i;
-
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
-}
-
-/**
  * print_string - prints a string to stdout
- * @args: va_list pointing to the number to print
+ * @args: va_list pointing to the string to print
  *
  * Return: the number of characters written to stdout
  */
@@ -89,9 +77,7 @@ int print_string(va_list args)
 	char *str = va_arg(args, char *);
 
 	if (str == NULL)
-	{
-		return (write(1, "(null)", 6));
-	}
+		str = "(null)";
 	return (write(1, str, _strlen(str)));
 }
 
@@ -107,29 +93,40 @@ int print_number(va_list args)
 	char buffer[20];
 	int i = 0, j, count = 0;
 
+	if (n == 0)
+	{
+		count += write(1, "0", 1);
+		return (count);
+	}
 	if (n < 0)
 	{
 		count += write(1, "-", 1);
 		n = -n;
 	}
-	do {
+	while (n > 0)
+	{
 		buffer[i] = n % 10 + '0';
 		i++;
 		n /= 10;
-	} while (n);
-	if (i < 2)
-	{
-		count += write(1, &buffer[i - 1], 1);
 	}
-	else
+	for (j = i - 1; j >= 0; j--)
 	{
-		for (j = i - 1; j >= 0; j--)
-		{
-			if (j == i - 1 && buffer[j] == '0')
-				continue;
-			count += write(1, &buffer[j], 1);
-		}
+		count += write(1, &buffer[j], 1);
 	}
 	return (count);
 }
 
+/**
+ * _strlen - gets the length of a string
+ * @str: the string to get the length of
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str && str[i])
+		i++;
+	return (i);
+}
